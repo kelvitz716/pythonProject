2827692f8a966a2cff3284f8f44486bdbba342c5
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-10 15:03:13 +0300

    added .gitignore

---
+++ Python intro.py
@@ -0,0 +1,8 @@
+title = "Welcome to Python"
+print(title)
+
+birth_year = int(input("what is your birth year?"))
+current_year = int(input("What's the year?"))
+age = current_year - birth_year
+print("your are " + str(age) + " years old")
+
