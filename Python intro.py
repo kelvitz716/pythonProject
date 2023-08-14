f5a8606cd78de62ecbb6e52da9b8c0c054b9b19f
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-14 12:34:51 +0300

    #print number to 100

--- Python intro.py
+++ Python intro.py
@@ -1,8 +1,28 @@
 title = "Welcome to Python"
 print(title)
 
+#calculate years
+import time
+
+current_year = int(time.strftime('%Y'))
 birth_year = int(input("what is your birth year?"))
-current_year = int(input("What's the year?"))
 age = current_year - birth_year
 print("your are " + str(age) + " years old")
+
+print("the", end=' ')
+print('big', end=' ')
+print("black", end=' ')
+print('box', end=' ')
+print("\n")
+
+#print number to 100
+i = int(input('Select your number'))
+
+while i<=100:
+    print(i)
+    i = i+5
+
+print("this is greater than 100")
+
+    
 
