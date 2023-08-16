d6cb63d84f74971fa2d47a75b3a72248dcd192e3
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-16 11:46:03 +0300

    loops

--- for loops.py
+++ for loops.py
@@ -1,4 +1,8 @@
+#print 0 to 9
 i = 1
 for i in range (0,10):
     print(str(i))
 print('the end')
+print('\n')
+
+#
--- while loops.py
+++ while loops.py
@@ -57,8 +57,7 @@ print('\n')
 start = int(input('What value should we start at? '))
 end = int(input('What value should we end at? '))
 divide = int(input('What number to check divisibility for? '))
-number = start
-while number < end:
-    if number % divide == 0:
-        print(number, 'is divisible by', divide)
-    number += 1
+while start < end:
+    if start % divide == 0:
+        print(start, 'is divisible by', divide)
+    start += 1
