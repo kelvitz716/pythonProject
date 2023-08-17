e54d94b75e3f81efea718d346c7fdec69fd5343c
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-17 11:20:36 +0300

    iterate through strings

---
+++ Iterate Through Strings.py
@@ -0,0 +1,19 @@
+#while loop
+string = 'The big brown fox'
+i = 0
+while i < len(string):
+    print(string[i])
+    i += 1
+print('\n')
+
+#for loop
+string = 'The big brown fox'
+for i in range(0, len(string)):
+    print(string[i])
+print('\n')
+
+#iterate through the characters
+string = 'The big brown fox'
+for char in string:
+    print(char)
+print('\n')
--- pythagorean.py
+++ pythagorean.py
@@ -2,7 +2,7 @@ from math import sqrt
 pythagorean = int(input("Enter your pythagorean number: "))
 for a in range(1, pythagorean + 1):
     for b in range(a, pythagorean + 1):
-        c_squared = a**2 + b**2
+        c_squared = (pow(a,2)) + (pow(b,2))
         c = int(sqrt(c_squared))
-        if c_squared - (c**2) == 0:
+        if c_squared - (pow(c,2)) == 0:
             print(str(a)+ ',' + str(b) + ',' +str(c))
