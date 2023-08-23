c47037210e32c38fb32bd90c80bddf26246b3e1d
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-23 10:42:54 +0300

    thesaurus

---
+++ thesaurus.py
@@ -0,0 +1,8 @@
+thesaurus = {}
+base_word = input('Enter the base word: ')
+similar_word = input('Enter a similar word: ')
+if base_word in thesaurus:
+    thesaurus[base_word].add(similar_word)
+else:
+    thesaurus[base_word] = {similar_word}
+print(thesaurus)
