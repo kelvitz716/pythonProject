1145bb73ff89d49d30ccc0717228993b907a875f
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-28 14:53:12 +0300

    program to cut and move file/dir

---
+++ move_file_or_dir.py
@@ -0,0 +1,14 @@
+import os
+
+
+source = "C:\\Users\\user\\Downloads\\IMG_20230423_082131.jpg" #put here the full file directory
+destination = "C:\\Users\\user\\OneDrive\\Pictures\\shee's.jpg" #put here the full destination directory
+
+try:
+    if os.path.exists(destination):
+        print("There's a similar file/dir in destination location: ")
+    else:
+        os.replace(source,destination)
+        print("File/dir moved. ")
+except ValueError as e:
+    print(e)
