08205a3e920d8292316fb3f3e9b9fd7f9ebd0ad6
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-29 10:11:31 +0300

    can be used to shortedn the while loop

---
+++ walrus_operator.py
@@ -0,0 +1,12 @@
+#foods = []
+
+#while True:
+#    food = input('What is your favourite food?: ')
+#    if food == "quit":
+#        break
+#    foods.append(food)
+
+foods = []
+
+while food := input('What is your favourite food?: ') !='quit':
+    foods.append(food)
