97f26e6567746717da096028e4fdfad8d1f6bd2c
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-08-29 10:44:35 +0300

    sorting a tuple inside a tuple

--- sort_method.py
+++ sort_method.py
@@ -1,11 +1,12 @@
-students = [("squidward","F","60"),
+students = (("squidward","F","60"),
             ("sandy","A","33"),
             ("patrick","D","36"),
             ("spongebob","B","20"),
-            ("Mr. krabs","C","78")]
+            ("Mr. krabs","C","78"))
 
 age = lambda age:age[2]
-students.sort(key=age,reverse=True)
+#students.sort(key=age,reverse=True) #this sorts a tuple inside a list
+sorted_students = sorted(students, reverse=True)    #this sorts a tuple inside an tuple
 for i in students:
     print(i)
 
