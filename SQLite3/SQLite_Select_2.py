453cff39cecf14ee7ca398e4ba6578a68b64ba6c
Gitau Kelvin <kelvitz716.kg@gmail.com>
2023-09-20 13:51:43 +0300

    select a certain value from a column(id) and return the whole row

---
+++ SQLite3/SQLite_Select_2.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+def getDevInfo(id):
+    try:
+        SQLite_connect = sqlite3.connect('sqlite3db.db')
+        cursor = SQLite_connect.cursor()
+        print('Sucessfully connected to DB')
+
+        SQLite_Select_Query = '''
+                        SELECT * FROM SqliteDB_Developers WHERE id = ?;'''
+        cursor.execute(SQLite_Select_Query, (id,))
+        records = cursor.fetchall()
+        print(f'The Selected Id is {id}')
+        for row in records:
+            print(f'''
+                Id: {row[0]}
+                Name: {row[1]}
+                Email: {row[2]}
+                Joining Date: {row[3]}
+                Salary: {row[4]}
+                \v''')
+            
+        cursor.close()
+
+    except Exception as e:
+        print(e)
+    
+    finally:
+        if SQLite_connect:
+            SQLite_connect.close()
+            print('DB connection closed')
+
+number = int(input("Enter the id you'd like information about: "))
+getDevInfo(number)
